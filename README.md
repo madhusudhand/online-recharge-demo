05aec52d3f884d10063dfe9395a83dc9711681a9
madhusudhand <madhusudhan.dollu@gmail.com>
2017-01-29 11:02:10 +0530

    readme

---
+++ README.md
@@ -0,0 +1,57 @@
+# Online Recharge Demo
+
+This project was implemented based on the following technologies.
+
+##### Front End
+- Angular 2 (with TypeScript)
+- Materialize css
+
+##### Backend
+- Node.js
+- Express
+
+## Setup
+
+To run this app, you need the following to be installed
+
+- Node.js (version 4+)
+
+Run the following commands to get started
+
+###### Backend
+
+```
+$ cd path/to/backend
+$ npm install
+```
+
+###### Front end
+
+```
+$ cd path/to/ui
+$ npm install
+$ npm install bower -g
+$ bower install
+```
+
+## Run your app
+
+**Backend**
+
+```
+$ cd path/to/backend
+$ npm start
+```
+
+This starts the backend app at http://localhost:3000.
+
+**Front end**
+
+```
+$ cd path/to/ui
+$ npm start
+```
+
+This starts the backend app at http://localhost:4200.
+
+Open the browser and navigate to http://localhost:4200.
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,13 +4,14 @@ var router = express.Router();
 const userService = require('../../db/user-service');
 const rechargeService = require('../../db/recharge-service');
 
+// default route
 router.get('/', function(req, res, next) {
   res.send({
     api: 'works!!'
   });
 });
 
-
+// route to get user details
 router.get('/user/:id', function(req, res, next) {
   var user = userService.getUser(req.params.id);
   res.send({
@@ -18,12 +19,15 @@ router.get('/user/:id', function(req, res, next) {
   });
 });
 
+// route to login
 router.post('/auth/user', function(req, res, next) {
   var user = userService.getUserLogin(req.body);
   res.send(user || 401);
 });
 
-
+// route to do recharge
+// this is same for both mobile and dth
+// thy differ with "kind" property
 router.post('/recharge', function(req, res, next) {
   var data = req.body.data;
   rechargeService.newRecharge({
@@ -38,6 +42,7 @@ router.post('/recharge', function(req, res, next) {
   res.send({status: 'success'});
 });
 
+// this is to get user recharge history
 router.get('/history/:userId', function(req, res, next) {
   var history = rechargeService.getRechargeHistory(req.params.userId);
   res.send(history || {});
